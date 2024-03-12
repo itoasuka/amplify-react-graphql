27bb1a0def561f4440ee76007c56011effbf3d62
Asuka ITO <itoasuka@gmail.com>
2024-03-12 17:31:01 +0900

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css"
 import "@aws-amplify/ui-react/styles.css";
-import {generateClient} from "aws-amplify/api";
+import {API, Storage} from "aws-amplify";
 import {
     withAuthenticator,
     Button,
@@ -9,7 +8,7 @@ import {
     Image,
     Text,
     View,
-    Card, Flex, TextField,
+    Flex, TextField,
 } from "@aws-amplify/ui-react";
 import {listNotes} from "./graphql/queries";
 import React, {useEffect, useState} from "react";
@@ -17,8 +16,6 @@ import {
     createNote as createNoteMutation,
     deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-
-const client = generateClient();
 
 function App({signOut}) {
     const [notes, setNotes] = useState([]);
@@ -28,19 +25,30 @@ function App({signOut}) {
     }, []);
 
     async function fetchNotes() {
-        const apiData = await client.graphql({query: listNotes})
+        const apiData = await API.graphql({query: listNotes})
         const notesFromAPI = apiData.data.listNotes.items;
+        await Promise.all(
+            notesFromAPI.map(async (note) => {
+                if (note.image) {
+                    const url = await Storage.get(note.name);
+                    note.image = url;
+                }
+            })
+        )
         setNotes(notesFromAPI)
     }
 
     async function createNote(event) {
         event.preventDefault();
         const form = new FormData(event.target);
+        const image = form.get("image")
         const data = {
             name: form.get("name"),
-            description: form.get("description")
+            description: form.get("description"),
+            image: image.name
         };
-        await client.graphql({
+        if (!!data.image) await Storage.put(data.name, image);
+        await API.graphql({
             query: createNoteMutation,
             variables: {input: data}
         });
@@ -48,10 +56,11 @@ function App({signOut}) {
         event.target.reset();
     }
 
-    async function deleteNote({id}) {
+    async function deleteNote({id, name}) {
         const newNotes = notes.filter((note) => note.id !== id);
         setNotes(newNotes);
-        await client.graphql({
+        await Storage.remove(name);
+        await API.graphql({
             query: deleteNoteMutation,
             variables: {input: {id}}
         })
@@ -78,6 +87,12 @@ function App({signOut}) {
                         variation="quiet"
                         required
                     />
+                    <View
+                        name="image"
+                        as="input"
+                        type="file"
+                        style={{alignSelf: "end"}}
+                        />
                     <Button type="submit" variation="primary">
                         Create Note
                     </Button>
@@ -96,6 +111,13 @@ function App({signOut}) {
                             {note.name}
                         </Text>
                         <Text as="span">{note.description}</Text>
+                        {note.image && (
+                            <Image
+                                src={note.image}
+                                alt={`visual aid for ${notes.name}`}
+                                style={{ width: 400 }}
+                            />
+                        )}
                         <Button variation="link" onClick={() => deleteNote(note)}>
                             Delete note
                         </Button>
