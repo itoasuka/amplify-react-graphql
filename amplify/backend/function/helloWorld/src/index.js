741f9fba856c1599746bbc14debc968eedb5afb0
Asuka ITO <itoasuka@gmail.com>
2024-03-14 14:27:39 +0900

    add credentials

--- amplify/backend/function/helloWorld/src/index.js
+++ amplify/backend/function/helloWorld/src/index.js
@@ -1,10 +1,16 @@
-
+const {GoogleAuth} = require('google-auth-library');
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    const auth = new GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/drive.metadata.readonly'],
+    });
+    console.log(auth)
+
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
